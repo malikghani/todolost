49f9747912310e8f67e80e3986e59de47ac8ae10
Ghani <hi@malikghani.com>
2024-04-19 23:37:27 +0700

    Update

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad(){
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
@@ -23,6 +23,10 @@ class ViewController: UIViewController {
     
     func createItems()   {
         print("creating the items")
+        
+        
+        
+        
         
         
         
